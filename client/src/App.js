2c40077cef04d3f8ca1fd12cf658c93c9e75e852
prince-kumar1504 <prince15raja04@gmail.com>
2023-07-19 16:04:43 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,27 @@
-
-import './App.css';
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
-//components 
-
-import Login from "./component/account/Login";
-import DataProvider from './context/DataProvider';
-import Home from './component/home/Home'
-import Header from './component/header/Header';
-
-
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Blogs from "./pages/Blogs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserBlogs from "./pages/UserBlogs";
+import CreateBlog from "./pages/CreateBlog";
+import BlogDetails from "./pages/BlogDetails";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
-
-    <DataProvider>
-      <BrowserRouter>
-      <Header/>
-      <div style={{ margin: 64 }}>
-        <Routes>
-        <Route path ='/login'element={ <Login/>} />
-        <Route path ='/'element={ <Home/>} />
-        </Routes>
-      </div>
-      </BrowserRouter>
-    </DataProvider >
-
+    <>
+      <Header />
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Blogs />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/my-blogs" element={<UserBlogs />} />
+        <Route path="/blog-details/:id" element={<BlogDetails />} />
+        <Route path="/create-blog" element={<CreateBlog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/component/header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div>Hello from Header</div>
-  )
-}
-
-export default Header
